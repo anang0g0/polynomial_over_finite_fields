c1043a960f1f24e7c2987d1a2fd8dc1dc962cb95
Osamu Sakai <fumumue@gmail.com>
2021-11-30 17:36:20 +0900

    del

--- lu.h
+++
@@ -1,9 +0,0 @@
-#define F 256*13 //2040
-
-extern unsigned char a[F][F];
-extern unsigned char cc[F][F];
-extern unsigned char bb[F][F];
-extern unsigned char cl[F][F];
-
-extern unsigned long xor128();
-  
