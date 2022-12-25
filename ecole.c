13ff5b1a117bcae30f838ce1d0b16675ed9a0414
Osamu Sakai <fumumue@protonmail.com>
2022-12-25 09:57:38 +0900

    format

--- ecole.c
+++ ecole.c
@@ -9,47 +9,47 @@
 
 static unsigned short gf[ORD] = {0}, fg[ORD] = {0};
 
-/* Generate nomal basis of Galois Field over GF(2^?) */
-static const unsigned int normal[15] = {
-    0b111,
-    0b1101,
-    0b11001,
-    0b110111,
-    0b1100001,
-    0b11000001,
-    0b110101001,
-    0b1100110001,
-    0b11000010011,
-    0b110000001101,
-    0b1100101000001,
-    0b11011000000001,
-    0b110000100010001,
-    0b1100000000000001,
-    0b11010000000010001};
-
-// Generate Sagemath based Galois Fields.
-static const unsigned int sage[15] = {
-    0b111,
-    0b1011,
-    0b10011,
-    0b100101,
-    0b1011011,
-    0b10000011,
-    0b100011101,      // sage
-    0b1000010001,     // sage512
-    0b10001101111,    // sage1024
-    0b100000000101,   // 2048
-    0b1000011101011,  /* sage 4096 */
-    0b10000000011011, /* Classic McEliece */
-    0b100000010101001,
-    0b1000000000110101,
-    0b10000000000101101};
-
-void gen_gf(unsigned int x, int n, int ord)
+void gen_gf(int n, int order)
 {
-    int i, j, k = x, count = 0;
-    FILE *fp;
-    char filename[8] = "";
+    int i, j, k, x, count = 0;
+
+    /* Generate nomal basis of Galois Field over GF(2^?) */
+    static const unsigned int normal[15] = {
+        0b111,
+        0b1101,
+        0b11001,
+        0b110111,
+        0b1100001,
+        0b11000001,
+        0b110101001,
+        0b1100110001,
+        0b11000010011,
+        0b110000001101,
+        0b1100101000001,
+        0b11011000000001,
+        0b110000100010001,
+        0b1100000000000001,
+        0b11010000000010001};
+
+    // Generate Sagemath based Galois Fields.
+    static const unsigned int sage[15] = {
+        0b111,
+        0b1011,
+        0b10011,
+        0b100101,
+        0b1011011,
+        0b10000011,
+        0b100011101,      // sage
+        0b1000010001,     // sage512
+        0b10001101111,    // sage1024
+        0b100000000101,   // 2048
+        0b1000011101011,  /* sage 4096 */
+        0b10000000011011, /* Classic McEliece */
+        0b100000010101001,
+        0b1000000000110101,
+        0b10000000000101101};
+
+    x = k = normal[n];
 
     while (k > 0)
     {
@@ -61,35 +61,42 @@ void gen_gf(unsigned int x, int n, int ord)
     gf[0] = 0;
     gf[1] = 1;
 
-    for (i = 2; i < ord; i++)
+    for (i = 2; i < order; i++)
     {
         if (gf[i] < k)
             gf[i] = (gf[i - 1] << 1);
         if (gf[i] >= k)
             gf[i] ^= x;
     }
-
-    sprintf(filename, "%d.h", ord);
-    fp = fopen(filename, "wb");
-
-    for (i = 0; i < ord; i++)
+    for (i = 0; i < order; i++)
     {
-        for (j = 0; j < ord; j++)
+        for (j = 0; j < order; j++)
         {
             if (gf[i] == j)
                 fg[j] = i;
         }
     }
+}
 
-    fprintf(fp, "static const unsigned short gf[%d]={\n", ord);
-    for (i = 0; i < ord - 1; i++)
+void put_gf(int order)
+{
+    FILE *fp;
+    char filename[8] = "";
+    int i;
+
+    sprintf(filename, "%d.h", order);
+    fp = fopen(filename, "wb");
+
+    int lastone = order - 1;
+    fprintf(fp, "static const unsigned short gf[%d]={\n", order);
+    for (i = 0; i < lastone; i++)
         fprintf(fp, "%d,", gf[i]);
-    fprintf(fp, "%d};\n", gf[ord - 1]);
+    fprintf(fp, "%d};\n", gf[lastone]);
 
-    fprintf(fp, "static const unsigned short fg[%d]={", ord);
-    for (i = 0; i < ord - 1; i++)
+    fprintf(fp, "static const unsigned short fg[%d]={", order);
+    for (i = 0; i < lastone; i++)
         fprintf(fp, "%d,", fg[i]);
-    fprintf(fp, "%d};\n", fg[ord - 1]);
+    fprintf(fp, "%d};\n", fg[lastone]);
 }
 
 int valid(int k)
@@ -126,9 +133,8 @@ int main(int argc, char *argv[])
 
     k = atoi(argv[1]);
     n = valid(k);
-    x = normal[n - 3];
 
-    gen_gf(x, n - 3, k);
-
+    gen_gf(n - 3, k);
+    put_gf(k);
     return 0;
 }
